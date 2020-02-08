cb1a7b57df08a5f2419829a40bc63f5c3199b24e
Andrey Kulikov <kulik.69@bk.ru>
2020-02-09 00:36:06 +0300

    homework1

---
+++ src/homework1/Task4.java
@@ -0,0 +1,12 @@
+package lesson1.homework1;
+
+public class Task4 {
+
+        public boolean task4(int a, int b) {
+            return a + b >= 10 && a + b <= 20;
+
+        }
+            }
+
+
+
---
+++ src/homework1/Task7.java
@@ -0,0 +1,10 @@
+package lesson1.homework1;
+
+public class Task7 {
+
+    public static void seventh(String name){
+        System.out.printf("Hey, %s!", name);
+    }
+    }
+
+
